3b624c14024b2e2512b30fe55185f4d75c196059
trasua <hyouka266@gmail.com>
2022-07-07 11:56:55 +0700

    finish thread pool

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,27 +1,41 @@
 use anyhow::Result;
 
-use std::sync::mpsc::channel;
+use std::sync::{
+    mpsc::{channel, Sender},
+    Arc, Mutex,
+};
+
+type Task = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
-    handles: Vec<std::thread::JoinHandle<()>>
+    sender: Sender<Task>,
+    workers: Vec<std::thread::JoinHandle<()>>,
 }
 
 impl ThreadPool {
-    pub fn new(num_threads: u8) -> Self {
-        let (sender, receiver): (Sender<()>, Receiver<()>) = channel::<()>();
-        let handles: Vec<std::thread::JoinHandle<()>> = (0..num_threads)
-            .map(|_| std::thread::spawn(|| {
-                loop {
-                
-                }
-            }))
-            .collect();
+    pub fn new(num_workers: u8) -> Self {
+        let (sender, receiver) = channel::<Task>();
+        let receiver = Arc::new(Mutex::new(receiver));
 
-        Self { handles }
+        let mut workers: Vec<std::thread::JoinHandle<()>> = Vec::with_capacity(num_workers.into());
+        for _ in 0..num_workers {
+            let rx = receiver.clone();
+            let worker = std::thread::spawn(move || loop {
+                let task = match rx.lock().unwrap().recv() {
+                    Ok(task) => task,
+                    Err(_) => break,
+                };
+                task();
+            });
+            workers.push(worker);
+        }
+
+        Self { sender, workers }
     }
 
-    pub fn execute<T: Fn()>(&self, task: T) -> Result<()> {
-        task();
-        Ok(())
+    pub fn execute<T: 'static + FnOnce() + Send>(&self, task: T) {
+        self.sender
+            .send(Box::new(task))
+            .expect("Thread shut down too early");
     }
 }
